821ede4c61f85345df466935fea7107330cc338a
bolubaj <bolu.bajomo@kandat.io>
2023-09-21 18:42:46 +0100

    + app list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 
 function App() {
+  const appNames = ["TAC Hymns"];
+
+  // Use the map function to render the list of app names
+  const appListItems = appNames.map((appName, index) => (
+    <li key={index}>{appName}</li>
+  ));
   return (
+    
     <div className="App">
-       <div>
-      <h1>Support Page</h1>
-      <p>Welcome to our support page. If you need assistance, please contact us at:</p>
-      <p>Email: development.btt@gmail.com</p>
-    </div>
+        <div>
+        <h1>Support Page</h1>
+        <p>Welcome to our support page. If you need assistance, please contact us at:</p>
+        <p>Email: development.btt@gmail.com</p>
+        <h2>Apps</h2>
+        <ul className="no-bullets-list">
+          {appListItems}
+        </ul>
+      </div>
     </div>
   );
 }
