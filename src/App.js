b70d1e2c6e570f621e26c91860dfdbdf7ccfd955
bolubaj <bolu.bajomo@kandat.io>
2023-09-21 18:36:41 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div>
+      <h1>Support Page</h1>
+      <p>Welcome to our support page. If you need assistance, please contact us at:</p>
+      <p>Email: development.btt@gmail.com</p>
+    </div>
     </div>
   );
 }
